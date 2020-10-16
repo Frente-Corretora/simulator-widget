81f978a672f5ff6bf33fb46f805ab917f1d3a816
Gustavo Agibert <gstv270@gmail.com>
2020-10-16 13:30:34 -0300

    Adds new currencies to outbound remittances

--- js/config.js
+++ js/config.js
@@ -31,5 +31,7 @@ const remittanceCurrenciesInbound = [
 const remittanceCurrenciesOutbound = [
   { name: 'Dólar Americano', code: 'USD', image: "https://s3.amazonaws.com/frente-exchanges/flags/united-states.svg" },
   { name: 'Euro', code: 'EUR', image: "https://s3.amazonaws.com/frente-exchanges/flags/european-union.svg" },
-  { name: 'Libra Esterlina', code: 'GBP', image: "https://s3.amazonaws.com/frente-exchanges/flags/united-kingdom.svg"}
+  { name: 'Libra Esterlina', code: 'GBP', image: "https://s3.amazonaws.com/frente-exchanges/flags/united-kingdom.svg"},
+  { name: 'Dólar Australiano', code: 'AUD', image: "https://s3.amazonaws.com/frente-exchanges/flags/australia.svg"},
+  { name: 'Dólar Canadense', code: 'CAD', image: "https://s3.amazonaws.com/frente-exchanges/flags/canada.svg"}
 ];
