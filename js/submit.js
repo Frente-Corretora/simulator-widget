bfd418783af35ac5a2a8706d02a5f9ee5ccd1a19
Kainan Guerra <kainan.guerra@frentecorretora.com.br>
2023-12-18 15:56:49 -0300

    fix: corrects url pattern to be equals to smiles

--- js/submit.js
+++ js/submit.js
@@ -12,7 +12,7 @@
 			const query = $('#exchange').serialize();
 			const currencyCode = window.exchange.value;
 			const merchantId = window.merchant.value;
-			const url = `https://iamsimple.com.br/${correspondent_identifier}/app/checkout/paper-money?${query}&agentId=${merchantId}&productId=${currencyCode}&paper-money=true`;
+			const url = `https://iamsimple.com.br/${correspondent_identifier}/?simulator=paperMoney&${query}&agentId=${merchantId}&productId=${currencyCode}`;
 			window.open(url, '_blank');
 		});
 
@@ -25,7 +25,7 @@
 			const currencyCode = window.remittance.value;
 			const purposeCode = window.beneficiary.value;
 			const remittanceType = window.remittanceType.value;
-			const url = `https://iamsimple.com.br/${correspondent_identifier}/app/checkout/remittance?remittance=true&${query}&purposeCode=${purposeCode}&currencyCode=${currencyCode}&remittanceType=${remittanceType}`;
+			const url = `https://iamsimple.com.br/${correspondent_identifier}/?simulator=remittance&${query}&purposeCode=${purposeCode}&currencyCode=${currencyCode}&remittanceType=${remittanceType}`;
 			window.open(url, '_blank');
 		});
 
